8b08ac2e3a9beb22e28df6e53add3e2160f0a880
aqi2015 <nisuosemoha@126.com>
2020-07-10 18:47:51 +0800

    add 88

---
+++ [88]合并两个有序数组.java
@@ -0,0 +1,87 @@
+//给你两个有序整数数组 nums1 和 nums2，请你将 nums2 合并到 nums1 中，使 nums1 成为一个有序数组。 
+//
+// 
+//
+// 说明: 
+//
+// 
+// 初始化 nums1 和 nums2 的元素数量分别为 m 和 n 。 
+// 你可以假设 nums1 有足够的空间（空间大小大于或等于 m + n）来保存 nums2 中的元素。 
+// 
+//
+// 
+//
+// 示例: 
+//
+// 输入:
+//nums1 = [1,2,3,0,0,0], m = 3
+//nums2 = [2,5,6],       n = 3
+//
+//输出: [1,2,2,3,5,6] 
+// Related Topics 数组 双指针 
+// 👍 557 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        if (m == 0) {
+            System.arraycopy(nums2, 0, nums1, 0, n);
+//            nums1 = nums2;
+            return;
+        } else if (n == 0) {
+            return;
+        }
+        int[] nums1_copy = new int[m];
+        System.arraycopy(nums1, 0, nums1_copy, 0, m);
+
+        int index1 = 0;
+        int index2 = 0;
+        int index3 = 0;
+        while (index1 < m && index2 < n) {
+            if (nums1_copy[index1] > nums2[index2]) {
+                nums1[index3] = nums2[index2];
+                index2++;
+            } else {
+                nums1[index3] = nums1_copy[index1];
+                index1++;
+            }
+            index3++;
+        }
+        if (index1 < m) {
+            System.arraycopy(nums1_copy, index1, nums1, index1 + index2, m - index1);
+        } else {
+            System.arraycopy(nums2, index2, nums1, index1 + index2, n - index2);
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+//官方
+//class Solution {
+//    public void merge(int[] nums1, int m, int[] nums2, int n) {
+//        // Make a copy of nums1.
+//        int [] nums1_copy = new int[m];
+//        System.arraycopy(nums1, 0, nums1_copy, 0, m);
+//
+//        // Two get pointers for nums1_copy and nums2.
+//        int p1 = 0;
+//        int p2 = 0;
+//
+//        // Set pointer for nums1
+//        int p = 0;
+//
+//        // Compare elements from nums1_copy and nums2
+//        // and add the smallest one into nums1.
+//        while ((p1 < m) && (p2 < n))
+//            nums1[p++] = (nums1_copy[p1] < nums2[p2]) ? nums1_copy[p1++] : nums2[p2++];
+//
+//        // if there are still elements to add
+//        if (p1 < m)
+//            System.arraycopy(nums1_copy, p1, nums1, p1 + p2, m + n - p1 - p2);
+//        if (p2 < n)
+//            System.arraycopy(nums2, p2, nums1, p1 + p2, m + n - p1 - p2);
+//    }
+//}
+
+
