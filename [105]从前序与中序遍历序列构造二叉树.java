bf8578397e302e2e6c3a2eb236314540c01a89f2
aqi2015 <nisuosemoha@126.com>
2020-07-23 23:19:41 +0800

    add 105

---
+++ [105]从前序与中序遍历序列构造二叉树.java
@@ -0,0 +1,52 @@
+//根据一棵树的前序遍历与中序遍历构造二叉树。 
+//
+// 注意: 
+//你可以假设树中没有重复的元素。 
+//
+// 例如，给出 
+//
+// 前序遍历 preorder = [3,9,20,15,7]
+//中序遍历 inorder = [9,3,15,20,7] 
+//
+// 返回如下的二叉树： 
+//
+//     3
+//   / \
+//  9  20
+//    /  \
+//   15   7 
+// Related Topics 树 深度优先搜索 数组 
+// 👍 579 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    private int pre = 0;
+    private int in = 0;
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        return build(preorder, inorder, Integer.MIN_VALUE);
+    }
+
+    private TreeNode build(int[] preorder, int[] inorder, int stop) {
+        if (pre >= preorder.length) return null;
+        if (inorder[in] == stop) {
+            in++;
+            return null;
+        }
+        TreeNode node = new TreeNode(preorder[pre++]);
+        node.left = build(preorder, inorder, node.val);
+        node.right = build(preorder, inorder, stop);
+        return node;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
